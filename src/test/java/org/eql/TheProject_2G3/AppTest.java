5580579d83172a6bd5ad789ee94e5d56dcceda6c
T4nnie <tannie.stovall@gmail.com>
2021-12-09 15:16:25 +0100

    Premier commit SOAP

--- src/test/java/org/eql/TheProject_2G3/AppTest.java
+++ src/test/java/org/eql/TheProject_2G3/AppTest.java
@@ -1,38 +1,70 @@
 package org.eql.TheProject_2G3;
 
-import org.apache.log4j.Logger;
+import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+
+//import org.apache.log4j.Logger;
+import org.apache.xmlbeans.XmlException;
+
+import com.eviware.soapui.impl.wsdl.WsdlProject;
+import com.eviware.soapui.model.support.PropertiesMap;
+import com.eviware.soapui.model.testsuite.TestRunner;
+import com.eviware.soapui.model.testsuite.TestSuite;
+import com.eviware.soapui.model.testsuite.TestCase;
+import com.eviware.soapui.support.SoapUIException;
 import com.eviware.soapui.tools.SoapUITestCaseRunner;
 
 import org.junit.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
+	//private final String restProject = "src/test/resources/REST-Simpleplan-soapui-project.xml" ;
+	private final String soapProject = "src/test/resources/SOAP-Logicaldoc-soapui-project.xml";
+	private final Logger LOGGER = Logger.getLogger(AppTest.class.getName());
+
+	/*@Test
+	public void testSimple() throws Exception {
+		SoapUITestCaseRunner testCaseRunner = new SoapUITestCaseRunner();
+		testCaseRunner.setProjectFile(restProject);
+		try {
+			testCaseRunner.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	*/
+	
 	@Test
-	public void test() throws Exception {
-	    SoapUITestCaseRunner testCaseRunner = new SoapUITestCaseRunner();
-	    testCaseRunner.setProjectFile("src/test/ressources/REST-Simpleplan-soapui-project.xml");
-	    try {
-	        testCaseRunner.run();
-	    } catch (Exception e) {
-	        e.printStackTrace();
+	public void testIntermediaire()
+	        throws XmlException, IOException, SoapUIException {
+		
+	    // Creer une nouvelle instance de WsdlProject en specifiant le chemin absolu du projet SoapUI
+	    WsdlProject project = new WsdlProject(soapProject);
+	    // Recupere tous les TestSuites inclus dans le projet SoapUI
+	     
+		
+	    List<TestSuite> testSuiteList = project.getTestSuiteList();
+	    // Iteration sur tous les TestSuites du projet
+	    for (TestSuite ts : testSuiteList) {
+	        LOGGER.info("******Running " + ts.getName() + "***********");
+	        // Recupere tous les TestCases d'une TestSuite
+	        List<TestCase> testCaseList = ts.getTestCaseList();
+	        // Iteration sur tous les TestCases d'un TestSuite particulier
+	        for (TestCase testcase : testCaseList) {
+	            LOGGER.info("******Running " + testcase.getName() + "***********");
+	            // Execute le TestCase specifie
+	            TestRunner testCaseRunner = testcase.run(new PropertiesMap(), false);
+	            // Verifie si le TestCase s'est termine correctement
+	            // ou s'il a echoue a cause d'un echec d'assertion
+	            assertEquals(TestRunner.Status.FINISHED, testCaseRunner.getStatus());
+	        }
 	    }
 	}
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
     
 }
